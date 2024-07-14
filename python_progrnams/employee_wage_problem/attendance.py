86c274aec5ad700085cc3f2608ed65b71cfb6855
Aniketwagh123 <106876220+Aniketwagh123@users.noreply.github.com>
2024-07-14 13:08:24 +0530

    [Aniket wagh]Add: Python script to check attendance

---
+++ python_progrnams/employee_wage_problem/attendance.py
@@ -0,0 +1,12 @@
+import random
+
+
+def check_attendance():
+    return random.choice([True, False])
+
+
+if __name__ == "__main__":
+    if check_attendance():
+        print("Employee is present.")
+    else:
+        print("Employee is absent.")
