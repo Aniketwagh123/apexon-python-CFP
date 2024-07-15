4bf88e57d6358b571bd774574486e236b2e355f2
Aniketwagh123 <106876220+Aniketwagh123@users.noreply.github.com>
2024-07-15 11:02:36 +0530

    [Aniket Wagh]Feat: Implemented daily employee wage calculation

--- python_progrnams/employee_wage_problem/employee_wage.py
+++ python_progrnams/employee_wage_problem/employee_wage.py
@@ -1,11 +1,15 @@
 import random
 
+WAGE_PER_HOUR = 20
+FULL_DAY_HOUR = 8
+
 def check_attendance():
     return random.choice([True, False])
 
 if __name__ == "__main__":
     print("Welcome to Employee Wage Computation Program")
     if check_attendance():
-        print("Employee is Present")
+        daily_wage = WAGE_PER_HOUR * FULL_DAY_HOUR
+        print(f"Daily Employee Wage: {daily_wage}")
     else:
         print("Employee is Absent")
